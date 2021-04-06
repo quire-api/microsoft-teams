7c907ab6eb703e0541b92861f0854868b732b917
galaxian85 <gasbomb_tw@hotmail.com>
2021-04-06 09:51:20 +0800

    Remove signup button

--- model/cardtemplates.js
+++ model/cardtemplates.js
@@ -91,11 +91,6 @@ class CardTemplates {
               value: `${process.env.DomainName}/bot-auth-start`
             }
           }
-        },
-        {
-          type: 'Action.OpenUrl',
-          title: 'Sign up',
-          url: 'https://quire.io/signup/'
         }
       ]
     });
